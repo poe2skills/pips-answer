1580c7168b645e02fb8f43aed57379ab8fa5a2ac
poe2skills <liqiaoa@gmail.com>
2026-01-20 20:33:32 +0800

    Update SlideOver.js

--- themes/heo/components/SlideOver.js
+++ themes/heo/components/SlideOver.js
@@ -99,8 +99,8 @@ export default function SlideOver(props) {
                         <div>{locale.COMMON.BLOG}</div>
                         {/* 导航按钮 */}
                         <div className='gap-2 grid grid-cols-2'>
-                          <Button title={'主页'} url={'/'} />
-                          <Button title={'关于'} url={'/about'} />
+                          <Button title={'HOME'} url={'/'} />
+                          <Button title={'ABOUT'} url={'/about'} />
                         </div>
                         {/* 用户自定义菜单 */}
                         <MenuListSide {...props} />
