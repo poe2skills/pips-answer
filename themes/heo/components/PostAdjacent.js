c9c117372896b7e6a05f4a1e15e1792c7a8b4a2c
poe2skills <liqiaoa@gmail.com>
2026-01-24 04:07:29 +0800

    Update PostAdjacent.js

--- themes/heo/components/PostAdjacent.js
+++ themes/heo/components/PostAdjacent.js
@@ -67,7 +67,7 @@ export default function PostAdjacent({ prev, next }) {
           href={`/${prev.slug}`}
           passHref
           className='cursor-pointer justify-between space-y-1 px-5 py-6 rounded-t-xl dark:bg-[#1e1e1e] border dark:border-gray-600 border-b-0 items-center dark:text-white flex flex-col w-full h-18 duration-200'>
-          <div className='flex justify-start items-center w-full'>上一篇</div>
+          <div className='flex justify-start items-center w-full'>Previous article</div>
           <div className='flex justify-center items-center text-lg font-bold'>
             {prev.title}
           </div>
@@ -76,7 +76,7 @@ export default function PostAdjacent({ prev, next }) {
           href={`/${next.slug}`}
           passHref
           className='cursor-pointer justify-between space-y-1 px-5 py-6 rounded-b-xl dark:bg-[#1e1e1e] border dark:border-gray-600 items-center dark:text-white flex flex-col w-full h-18 duration-200'>
-          <div className='flex justify-start items-center w-full'>下一篇</div>
+          <div className='flex justify-start items-center w-full'>Next article</div>
           <div className='flex justify-center items-center text-lg font-bold'>
             {next.title}
           </div>
